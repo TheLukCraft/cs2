5aecaf1c3d9f61fce228f2e72cd5acadbb0b3e14
TheLukCraft <luk1997r@o2.pl>
2023-04-13 01:49:26 +0200

    service installation problem

---
+++ WebAPI/Installers/InstallExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace WebAPI.Installers
+{
+    public static class InstallExtensions
+    {
+        public static void InstallServicesInAssembly(this IServiceCollection services, IConfiguration configuration)
+        {
+            var installers = typeof(StartupBase).Assembly.ExportedTypes.Where(x =>
+            typeof(IInstaller).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract).Select(Activator.CreateInstance).Cast<IInstaller>();
+
+            foreach (var installer in installers)
+            {
+                installer.InstallServices(services, configuration);
+            }
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,34 +1,25 @@
-using Application.Interfaces;
-using Application.Mappings;
-using Application.Services;
-using Domain.Interfaces;
-using Infrastructure.Repositories;
-using Microsoft.OpenApi.Models;
+using WebAPI.Installers;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<IPostRepository, PostRepository>();
-builder.Services.AddScoped<IPostService, PostService>();
-
-builder.Services.AddSingleton(AutoMapperConfig.Initialize());
-
+builder.Services.InstallServicesInAssembly(builder.Configuration);
+builder.Services.AddAuthorization();
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.EnableAnnotations();
-});
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+// Configure the HTTP request pipeline.
+
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
