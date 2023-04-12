6cc5271990ba1d7c8f72a345e97c8dc9522592ce
TheLukCraft <luk1997r@o2.pl>
2023-04-12 21:08:26 +0200

    Add project files.

---
+++ Domain/Entities/Post.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Post : AuditableEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public Post()
+        {
+        }
+
+        public Post(int id, string title, string content)
+        {
+            (Id, Title, Content) = (id, title, content);
+        }
+    }
+}
---
+++ Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+
+namespace Infrastructure.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+        private static readonly ISet<Post> posts = new HashSet<Post>()
+        {
+            new Post(1, "test 1", "treść 1"),
+            new Post(2, "test 2", "treść 2"),
+            new Post(3, "test 3", "treść 3"),
+        };
+
+        public IEnumerable<Post> GetAll()
+        {
+            return posts;
+        }
+
+        public Post GetById(int id)
+        {
+            return posts.SingleOrDefault(x => x.Id == id);
+        }
+
+        public Post Add(Post post)
+        {
+            post.Id = posts.Count() + 1;
+            post.Created = DateTime.UtcNow;
+            posts.Add(post);
+            return post;
+        }
+
+        public void Updated(Post post)
+        {
+            post.LastModified = DateTime.UtcNow;
+        }
+
+        public void Delete(Post post)
+        {
+            posts.Remove(post);
+        }
+    }
+}
