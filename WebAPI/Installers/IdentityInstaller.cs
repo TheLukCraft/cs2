8c880edcee6895cdcc6b63168a81872c2a742ae7
TheLukCraft <luk1997r@o2.pl>
2023-04-15 04:41:52 +0200

    JWT Settings

---
+++ WebAPI/Installers/IdentityInstaller.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace WebAPI.Installers
+{
+    public class IdentityInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection Services, IConfiguration Configuration)
+        {
+            Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Secret"]))
+                };
+            });
+        }
+    }
+}
