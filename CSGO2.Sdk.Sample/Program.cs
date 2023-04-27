193918558a5cbed18c1c3a40a0a11b7d1c3fe90d
TheLukCraft <luk1997r@o2.pl>
2023-04-27 11:25:52 +0200

    Revert "Revert "using CSGO2.Contracts.Requests;""
    This reverts commit fa5cb8871905d7d4a7caa73f362267ce12c01294.

---
+++ CSGO2.Sdk.Sample/Program.cs
@@ -0,0 +1,50 @@
+﻿using CSGO2.Contracts.Requests;
+using CSGO2.Contracts.Responses;
+using Refit;
+
+namespace CSGO2.Sdk.Sample
+{
+    public class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var cachedToken = string.Empty;
+            var identityApi = RestService.For<IIdentityAPI>("https://localhost:7138/");
+            var csgoApi = RestService.For<ICSGO2API>("https://localhost:7138/", new RefitSettings
+            {
+                AuthorizationHeaderValueGetter = () => Task.FromResult(cachedToken)
+            });
+
+            var register = await identityApi.RegisterAsync(new RegisterModel()
+            {
+                Email = "sdkAcc@gmail.com",
+                UserName = "sdkAcc",
+                Password = "Pa$$word123!"
+            });
+
+            var login = await identityApi.LoginAsync(new LoginModel()
+            {
+                UserName = "sdkAcc",
+                Password = "Pa$$word123!"
+            });
+
+            cachedToken = login.Content.Token;
+
+            var createdPost = await csgoApi.CreatePostAsync(new CreatePostDto
+            {
+                Title = "Post sdk",
+                Content = "treść sdk"
+            });
+
+            var retrievedPost = await csgoApi.GetPostAsync(createdPost.Content.Data.Id);
+
+            await csgoApi.UpdatePostAsync(new UpdatePostDto
+            {
+                Id = retrievedPost.Content.Data.Id,
+                Content = "nowa treść sdk"
+            });
+
+            await csgoApi.DeletePostAsync(retrievedPost.Content.Data.Id);
+        }
+    }
+}
