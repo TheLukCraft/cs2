38c4c923752d207be00169d368ac1250ef197af9
TheLukCraft <luk1997r@o2.pl>
2023-04-27 16:46:45 +0200

    Added Elastic search

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -2,6 +2,9 @@ using Infrastructure.Data;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System.Security.Policy;
 using WebAPI.HealthChecks;
 using WebAPI.Installers;
 using WebAPI.Middelwares;
@@ -11,6 +14,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.InstallServicesInAssembly(builder.Configuration);
 builder.Services.AddAuthorization();
 builder.Services.AddControllers();
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration.Enrich.FromLogContext()
+    .Enrich.WithMachineName()
+    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticConfiguration:Uri"]))
+    {
+        IndexFormat = $"{context.Configuration["ApplicationName"]}-logs-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}",
+        AutoRegisterTemplate = true,
+        NumberOfShards = 2,
+        NumberOfReplicas = 1
+    })
+    .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+    .ReadFrom.Configuration(context.Configuration);
+});
 
 builder.Services.AddDbContext<CSGOContext>(
     option => option.UseSqlServer(builder.Configuration.GetConnectionString("CSGODb"))
