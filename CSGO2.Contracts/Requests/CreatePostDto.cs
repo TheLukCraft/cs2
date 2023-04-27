1dff54d135273db1a8c4769976320ac4514ce6d3
TheLukCraft <luk1997r@o2.pl>
2023-04-27 10:13:33 +0200

    using CSGO2.Contracts.Requests;

---
+++ CSGO2.Contracts/Requests/CreatePostDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CSGO2.Contracts.Requests
+{
+    public class CreatePostDto
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ CSGO2.Sdk/IIdentityAPI.cs
@@ -0,0 +1,19 @@
+﻿using CSGO2.Contracts;
+using CSGO2.Contracts.Requests;
+using CSGO2.Contracts.Responses;
+using Refit;
+
+namespace CSGO2.Sdk
+{
+    public interface IIdentityAPI
+    {
+        [Post("/identity/register")]
+        Task<ApiResponse<Response>> RegisterAsync([Body] RegisterModel register);
+
+        [Post("/identity/registerAdmin")]
+        Task<ApiResponse<Response>> RegisterAdminAsync([Body] RegisterModel register);
+
+        [Post("/identity/login")]
+        Task<ApiResponse<AuthSuccessResponse>> LoginAsync([Body] LoginModel login);
+    }
+}
