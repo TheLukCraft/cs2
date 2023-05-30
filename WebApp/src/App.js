55d2b7372122f414829e63caebb8e6454b5704d7
Alicja Begović <alicja.begovic@gmail.com>
2023-05-30 13:20:20 +0200

    maps and styles

--- WebApp/src/App.js
+++ WebApp/src/App.js
@@ -1,14 +1,13 @@
 import Header from "./components/Header/Header";
 import Description from "./components/Description/Description";
+import Maps from "./components/Maps/Maps";
 
 function App() {
   return (
     <div>
       <Header />
       <Description />
-      <h3>Mirage</h3>
-      <h3>Inferno</h3>
-      <h3>Overpass</h3>
+      <Maps />
     </div>
   );
 }
