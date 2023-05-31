7c76c4bc0a96afe982ded8ab815877f00bf622b4
Alicja Begović <alicja.begovic@gmail.com>
2023-06-01 01:47:19 +0200

    maps position with grid

--- WebApp/src/components/Maps/Maps.js
+++ WebApp/src/components/Maps/Maps.js
@@ -4,19 +4,15 @@ import classes from "./Maps.module.css";
 
 import Mirage from "../../img/Mirage.png";
 import Inferno from "../../img/Inferno.png";
-import Overpass from "../../img/Overpass.png";
 
 export default function Maps() {
   return (
     <div className={classes.container}>
-      <div className={classes.imageMirage}>
+      <div className={classes.image}>
         <img className={classes.img} src={Mirage} alt="Mirage Map" />
       </div>
       <div className={classes.image}>
         <img className={classes.img} src={Inferno} alt="Inferno Map" />
-      </div>
-      <div className={classes.image}>
-        <img className={classes.img} src={Overpass} alt="Overpass Map" />
       </div>
     </div>
   );
