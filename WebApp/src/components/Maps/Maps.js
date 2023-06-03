859bf374309ec59ec0bcd933bfa110aeaa4d0293
Alicja Begović <alicja.begovic@gmail.com>
2023-06-04 00:49:36 +0200

    all maps uploaded

--- WebApp/src/components/Maps/Maps.js
+++ WebApp/src/components/Maps/Maps.js
@@ -4,6 +4,11 @@ import classes from "./Maps.module.css";
 
 import Mirage from "../../img/Mirage.png";
 import Inferno from "../../img/Inferno.png";
+import Overpass from "../../img/Overpass.png";
+import Ancient from "../../img/Ancient.png";
+import Nuke from "../../img/Nuke.png";
+import Vertigo from "../../img/Vertigo.png";
+import Anubis from "../../img/Anubis.png";
 
 export default function Maps() {
   return (
@@ -15,16 +20,19 @@ export default function Maps() {
         <img className={classes.img} src={Inferno} alt="Inferno Map" />
       </div>
       <div className={classes.image}>
-        <img className={classes.img} src={Inferno} alt="Inferno Map" />
+        <img className={classes.img} src={Overpass} alt="Overpass Map" />
       </div>
       <div className={classes.image}>
-        <img className={classes.img} src={Inferno} alt="Inferno Map" />
+        <img className={classes.img} src={Ancient} alt="Ancient Map" />
       </div>
       <div className={classes.image}>
-        <img className={classes.img} src={Inferno} alt="Inferno Map" />
+        <img className={classes.img} src={Nuke} alt="Nuke Map" />
       </div>
       <div className={classes.image}>
-        <img className={classes.img} src={Inferno} alt="Inferno Map" />
+        <img className={classes.img} src={Vertigo} alt="Vertigo Map" />
+      </div>
+      <div className={classes.image}>
+        <img className={classes.img} src={Anubis} alt="Anubis Map" />
       </div>
     </div>
   );
