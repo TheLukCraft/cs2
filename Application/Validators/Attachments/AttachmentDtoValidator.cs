caa60649e3c714e920e4b82fbd7e649c35a33977
TheLukCraft <luk1997r@o2.pl>
2023-10-09 09:20:33 +0200

    Added validation of attachment class

---
+++ Application/Validators/Attachments/AttachmentDtoValidator.cs
@@ -0,0 +1,24 @@
+﻿using Application.Dto.Attachments;
+using FluentValidation;
+
+namespace Application.Validators.Attachments
+{
+    public class AttachmentDtoValidator : AbstractValidator<AttachmentDto>
+    {
+        public AttachmentDtoValidator()
+        {
+            #region Id
+
+            RuleFor(x => x.Id).NotEmpty().WithMessage("There was an error with the attachment ID");
+
+            #endregion Id
+
+            #region Name
+
+            RuleFor(x => x.Name).NotEmpty().WithMessage("The name of the attachment must be given");
+            RuleFor(x => x.Name).Length(3, 100).WithMessage("The length of the attachment name must be more than 3 characters, but must not exceed 100.");
+
+            #endregion Name
+        }
+    }
+}
---
+++ Application/Validators/Attachments/DownloadAttachmentDtoValidator.cs
@@ -0,0 +1,24 @@
+﻿using Application.Dto.Attachments;
+using FluentValidation;
+
+namespace Application.Validators.Attachments
+{
+    public class DownloadAttachmentDtoValidator : AbstractValidator<DownloadAttachmentDto>
+    {
+        public DownloadAttachmentDtoValidator()
+        {
+            #region Name
+
+            RuleFor(x => x.Name).NotEmpty().WithMessage("The name of the attachment must be given");
+            RuleFor(x => x.Name).Length(3, 100).WithMessage("The length of the picture name must be more than 3 characters, but must not exceed 100.");
+
+            #endregion Name
+
+            #region Content
+
+            RuleFor(x => x.Content).NotEmpty().WithMessage("the contents of the attachment must not be empty");
+
+            #endregion Content
+        }
+    }
+}
