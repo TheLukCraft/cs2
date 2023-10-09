561a4a335f1bf053f49a21d38fed79f3e2cfd203
TheLukCraft <luk1997r@o2.pl>
2023-10-09 09:06:53 +0200

    added support for validation in the picture class

---
+++ Application/Validators/Picture/PictureDtoValidator.cs
@@ -0,0 +1,30 @@
+﻿using Application.Dto.Picture;
+using FluentValidation;
+
+namespace Application.Validators.Picture
+{
+    public class PictureDtoValidator : AbstractValidator<PictureDto>
+    {
+        public PictureDtoValidator()
+        {
+            #region Id
+
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Need map ID and appropriate permissions");
+
+            #endregion Id
+
+            #region Name
+
+            RuleFor(x => x.Name).NotEmpty().WithMessage("The name of the picture must be given");
+            RuleFor(x => x.Name).Length(3, 100).WithMessage("The length of the picture name must be more than 3 characters, but must not exceed 100.");
+
+            #endregion Name
+
+            #region Image
+
+            RuleFor(x => x.Image).NotEmpty().WithMessage("The image does not contain any byte array.");
+
+            #endregion Image
+        }
+    }
+}
