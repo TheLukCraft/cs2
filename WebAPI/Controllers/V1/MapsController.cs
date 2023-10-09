a24adfb00f4d48f3d542f95a6b24166af48e9165
TheLukCraft <luk1997r@o2.pl>
2023-10-09 10:53:40 +0200

    Completed documentation in class maps

--- WebAPI/Controllers/V1/MapsController.cs
+++ WebAPI/Controllers/V1/MapsController.cs
@@ -24,6 +24,10 @@ namespace WebAPI.Controllers.V1
             this.mapService = mapService;
         }
 
+        /// <summary>
+        /// Retrieves all maps asynchronously.
+        /// </summary>
+        /// <Response code="200">(OK) with the collection of maps if retrieval is successful.</Response>
         [SwaggerOperation(Summary = "Retrieves all maps")]
         [AllowAnonymous]
         [HttpGet("[action]")]
@@ -33,6 +37,12 @@ namespace WebAPI.Controllers.V1
             return Ok(maps);
         }
 
+        /// <summary>
+        /// Retrieves a specific map by its unique ID asynchronously.
+        /// </summary>
+        /// <param name="id">The unique identifier of the map to retrieve.</param>
+        /// <Reseponse code="404">(Not Found) if the map with the specified ID does not exist.</Reseponse>
+        /// <Response code="200">(OK) with the MapDto object if the retrieval is successful.</Response>
         [SwaggerOperation(Summary = "Retrieves a specific map")]
         [AllowAnonymous]
         [HttpGet("{id}")]
@@ -45,6 +55,12 @@ namespace WebAPI.Controllers.V1
             return Ok(new Response<MapDto>(map));
         }
 
+        /// <summary>
+        /// Adds a new map asynchronously.
+        /// </summary>
+        /// <param name="newMap">The CreateMapDto object containing the details of the new map.</param>
+        /// <Response code="401">(Unauthorized) if the user is not authorized (requires Admin role).</Response>
+        /// <Resposne code="201">(Created) with the MapDto object if the map is successfully added.</Resposne>
         [SwaggerOperation(Summary = "Add new map")]
         [Authorize(Roles = UserRoles.Admin)]
         [HttpPost]
@@ -54,6 +70,12 @@ namespace WebAPI.Controllers.V1
             return Created($"maps/{map.Id}", new Response<MapDto>(map));
         }
 
+        /// <summary>
+        /// Updates an existing map asynchronously.
+        /// </summary>
+        /// <param name="updateMap">The UpdateMapDto object containing the updated details of the map.</param>
+        /// <Response code="401">(Unauthorized) if the user is not authorized (requires Admin role).</Response>
+        /// <Response code="204"> (No Content) if the map is successfully updated.</Response>
         [SwaggerOperation(Summary = "Update a existing map")]
         [Authorize(Roles = UserRoles.Admin)]
         [HttpPut()]
@@ -63,6 +85,12 @@ namespace WebAPI.Controllers.V1
             return NoContent();
         }
 
+        /// <summary>
+        /// Deletes a specific map by its unique ID asynchronously.
+        /// </summary>
+        /// <param name="id">The unique identifier of the map to delete.</param>
+        /// <Response code="401">(Unauthorized) if the user is not authorized (requires Admin role).</Response>
+        /// <Response code="204">(No Content) if the map is successfully deleted.</Response>
         [SwaggerOperation(Summary = "Delete a specific map")]
         [Authorize(Roles = UserRoles.Admin)]
         [HttpDelete("{id}")]
